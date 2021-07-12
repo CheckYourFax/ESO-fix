dddf1fac43c5aa35dcbf25efc40c9381dedf1116
CheckYourFax <75031747+CheckYourFax@users.noreply.github.com>
2021-07-12 13:02:38 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         ESO Forum Fix
+// @name         ESO Forum visibility Fix
 // @namespace    http://tampermonkey.net/
-// @version      1.02
-// @description  Fixes ESO forums with a simple hack to temporarily work around a bug within the ESO forums.
+// @version      1.03
+// @description  Fixes ESO forums with a simple CSS hack to temporarily work around a visibility bug within the ESO forums.
 // @include      https://forums.elderscrollsonline.com/*
 // @author       CheckYourFax
 // @icon         https://us.v-cdn.net/5020507/uploads/favicon_07701a7cad522642.ico
 // @grant        none
 // ==/UserScript==
 window.addEventListener('load', function() {
-javascript:(function(){var a=document.createElement('style'),b;document.head.appendChild(a);b=a.sheet;b.insertRule('body{visibility:visible !important;}',0);})()
+(function(){var a=document.createElement('style'),b;document.head.appendChild(a);b=a.sheet;b.insertRule('body{visibility:visible !important;}',0);})();
 }, false);
 
 // Courtesy of CheckYourFax#1445 of the Daedric Banditoos
