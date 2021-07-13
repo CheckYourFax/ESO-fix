06491078bc5801c0d6c3a74d2ff41581d00207ec
CheckYourFax <75031747+CheckYourFax@users.noreply.github.com>
2021-07-13 15:04:03 +0200

    Update user.js

--- user.js
+++ user.js
@@ -2,14 +2,17 @@
 // @name         ESO Forum visibility Fix
 // @namespace    http://tampermonkey.net/
 // @version      1.04
-// @description  Fixes ESO forums with a simple CSS hack to temporarily work around a visibility bug within the ESO forums.
+// @description  Fixes ESO forums with a simple CSS hack to temporarily work around a visibility bug within the ESO forums
 // @include      https://forums.elderscrollsonline.com/*
 // @author       CheckYourFax
 // @icon         https://us.v-cdn.net/5020507/uploads/favicon_07701a7cad522642.ico
 // @grant        none
 // ==/UserScript==
-window.addEventListener('load', function() { //Wait for page to load
-(function(){var a=document.createElement('style'),b;document.head.appendChild(a);b=a.sheet;b.insertRule('body{visibility:visible !important;}',0);})(); //overwrites the style element visibility to remove 'hidden' element.
+window.addEventListener('load', function() {
+(function(){var a=document.createElement('style'),b;document.head.appendChild(a);b=a.sheet;b.insertRule('body{visibility:visible !important;}',0);})();
 }, false);
+//First it waits for page to load (line 11, 13)
+//The script then adds (and thus overwrites) the property 'visibility' within the 'body' selector, but without the 'hidden' value added to the 'visibility' property
+
 // This script should not interfere with normal forum functionality when this bug is ultimately fixed by Bethesda/ZeniMax since it only overwrites an element that should not have been there in the first place
 // Courtesy of CheckYourFax#1445 of the Daedric Banditoos
