1c3db73a8e92a1923330b33f6d3e5ccaeecaabce
CheckYourFax <75031747+CheckYourFax@users.noreply.github.com>
2021-07-13 15:09:29 +0200

    final

--- user.js
+++ user.js
@@ -11,8 +11,8 @@
 window.addEventListener('load', function() {
 (function(){var a=document.createElement('style'),b;document.head.appendChild(a);b=a.sheet;b.insertRule('body{visibility:visible !important;}',0);})();
 }, false);
-//First it waits for page to load (line 11, 13)
-//The script then adds (and thus overwrites) the property 'visibility' within the 'body' selector, but without the 'hidden' value added to the 'visibility' property
+//First the script waits for page to load (line 11, 13)
+//Then it adds (and thus overwrites) the property 'visibility' within the 'body' selector, but without the 'hidden' value added to the 'visibility' property (line 12)
 
 // This script should not interfere with normal forum functionality when this bug is ultimately fixed by Bethesda/ZeniMax
 // Courtesy of CheckYourFax#1445 of the Daedric Banditoos
